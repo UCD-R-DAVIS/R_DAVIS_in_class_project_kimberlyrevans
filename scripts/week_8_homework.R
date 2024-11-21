985cf5e1baa200fd695e6aaff99e94877a2b3b85
kimberlyrevans <krevans@ucdavis.edu>
2024-11-21 13:46:32 -0800

    additions to hw 8

--- scripts/week_8_homework.R
+++ scripts/week_8_homework.R
@@ -13,9 +13,9 @@ mloa <- read_csv("https://raw.githubusercontent.com/gge-ucd/R-DAVIS/master/data/
 
 #remove observations with missing values in rel_humid, temp_C_2m, and windSpeed_m_s.
 mloa_filtered<- mloa%>%
-  filter(!windSpeed_m_s == "-999.9")%>%
-  filter(!temp_C_2m== "-999.9")%>%
-  filter(!rel_humid== "-99")
+  filter(windSpeed_m_s != "-999.9")%>%
+  filter(temp_C_2m != "-999.9")%>%
+  filter(rel_humid != "-99")
 
 #Generate a column called “datetime” using the year, month, day, hour24, and min columns.
 
@@ -26,6 +26,18 @@ mloa_filtered<- mloa%>%
   mutate(datetime= ymd_hm(paste(year, "-", month,
                                 "-", day, ", ", hour24, ":",
                                 min, sep = "")))
+
+
+#other option
+mloa_filtered<- mloa%>%
+  filter(!windSpeed_m_s == "-999.9")%>%
+  filter(!temp_C_2m== "-999.9")%>%
+  filter(!rel_humid== "-99")%>%
+  mutate(datetime= ymd_hm(paste(year, month, day, hour24, min, sep = "-")))
+
+#or
+mutate(datetime = ymd_hm(paste(paste(year,month,day,sep= "-"), paste(hour24,min,sep=":"))))
+
 
 #Next, create a column called “datetimeLocal” that converts the datetime column 
 #to Pacific/Honolulu time (HINT: look at the lubridate function called with_tz())
@@ -48,8 +60,11 @@ mloa_meantemp<- mloa_filtered%>%
   summarise(temp=mean(temp_C_2m))
 
 ggplot(data = mloa_meantemp)+
-  geom_point(aes(x=monthlocal, y= temp, col= hourlocal))+
+  geom_point(aes(x=month(monthlocal, label =T), y= temp, col= hourlocal))+
   scale_colour_viridis_c() +
   xlab("Month") +
   ylab("Average temperature") +
   theme_dark()
+#can change numbers to months by doing month([monthcolumn], label=T)
+# another palette:
+# scale_color_distiller(palette = "Oranges")
